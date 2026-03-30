9fee0a5d6e3daed181225464e204669785ec01b8
Nikhil gupta <ng364357@gmail.com>
2026-03-30 18:39:13 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/src/features/home/services/game.service.js
+++ Frontend/src/features/home/services/game.service.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://negotation-game.onrender.com/"
 })
 
 export const startGame = async (productName, productImage, basePrice) => {
